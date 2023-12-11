768983ac76bc84bc28aba8b32f8ec164f51c63e4
chuck <403chuck@gmail.com>
2023-12-12 00:01:19 +0800

    feat: add zhihu trending

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-interface WeiboDate {
+interface Word {
   url: string
   title: string
 }
 
-async function getWeiboDate() {
-  const regexp = /<a href="(\/weibo\?q=[^"]+)".*?>(.+)<\/a>/g
-
-  const response = await fetch('https://s.weibo.com/top/summary', {
+async function getWeiboData() {
+  const response = await fetch('https://weibo.com/ajax/side/hotSearch', {
     headers: {
       Cookie:
         'SUB=_2AkMWJrkXf8NxqwJRmP8SxWjnaY12zwnEieKgekjMJRMxHRl-yj9jqmtbtRB6PaaX-IGp-AjmO6k5cS-OH2X9CayaTzVD',
@@ -15,11 +13,28 @@ async function getWeiboDate() {
   if (!response.ok) {
     throw new Error(response.statusText)
   }
-  const result = await response.text()
-  const matches = result.matchAll(regexp)
-  const words: WeiboDate[] = Array.from(matches).map((x) => ({
-    url: x[1],
-    title: x[2],
+  const { data, ok } = await response.json()
+  if (ok !== 1) {
+    throw new Error('response not ok')
+  }
+  const words: Word[] = data?.realtime
+    .filter((item) => item.ad_channel !== 1)
+    .map((item) => ({
+      url: `https://s.weibo.com/weibo?q=${item.word_scheme}`,
+      title: item.word,
+    }))
+  return words
+}
+
+async function getZhihuData() {
+  const response = await fetch('https://www.zhihu.com/api/v4/search/top_search')
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  const { top_search } = await response.json()
+  const words: Word[] = top_search?.words.map((item) => ({
+    url: `https://www.zhihu.com/search?q=${item.query}`,
+    title: item.display_query,
   }))
   return words
 }
@@ -31,11 +46,12 @@ export default async function Page({
   params: { slug: string }
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  const weiboData = await getWeiboDate()
+  const weiboData = await getWeiboData()
+  const zhihuData = await getZhihuData()
 
   return (
     <main className="container mx-auto px-5 py-10">
-      <section className="w-full pb-12 lg:pb-32 xl:pb-48">
+      <section className="w-full pb-8 lg:pb-18 xl:pb-22">
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
@@ -57,10 +73,9 @@ export default async function Page({
       <section className="py-5">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
-            className="rounded-lg border bg-card text-card-foreground shadow-sm"
-            data-v0-t="card"
+            className="rounded-lg border bg-card text-card-foreground shadow-sm p-5"
           >
-            <div className="flex flex-col space-y-1.5 p-6">
+            <div className="flex flex-col space-y-1.5 pb-3">
               <h3 className="text-2xl font-semibold leading-none tracking-tight">
                 Weibo Hot Search
               </h3>
@@ -68,11 +83,41 @@ export default async function Page({
                 Discover the Weibo latest trends.
               </p>
             </div>
-            <div className="p-6 space-y-2">
+            <div className="space-y-2">
               {weiboData.map((x, i) => (
                 <a
                   key={x.url}
-                  href={`https://s.weibo.com${x.url}`}
+                  href={`${x.url}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex items-center space-x-3"
+                >
+                  <span className="text-sm font-medium text-primary-foreground">
+                    {i + 1}.
+                  </span>
+                  <span className="text-sm font-medium text-primary-foreground">
+                    {x.title}
+                  </span>
+                </a>
+              ))}
+            </div>
+          </div>
+          <div
+            className="rounded-lg border bg-card text-card-foreground shadow-sm p-5"
+          >
+            <div className="flex flex-col space-y-1.5 pb-3">
+              <h3 className="text-2xl font-semibold leading-none tracking-tight">
+                Zhihu Hot Search
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                Discover the Zhihu latest trends.
+              </p>
+            </div>
+            <div className="space-y-2">
+              {zhihuData.map((x, i) => (
+                <a
+                  key={x.url}
+                  href={`${x.url}`}
                   target="_blank"
                   rel="noreferrer"
                   className="flex items-center space-x-3"
@@ -92,3 +137,5 @@ export default async function Page({
     </main>
   )
 }
+
+export const revalidate = 1800
