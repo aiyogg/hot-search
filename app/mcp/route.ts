e13cf2617ae6f8902ec68e8fbece4700d6215b63
Chuck <403chuck@gmail.com>
2025-04-25 00:05:04 +0800

    feat: add support for MCP server

---
+++ app/mcp/route.ts
@@ -0,0 +1,75 @@
+import { createServerResponseAdapter } from '../../lib/server-response-adapter'
+import { initializeMcpApiHandler } from '../../lib/mcp-api-handler'
+import { CallToolResult } from '@modelcontextprotocol/sdk/types'
+import { getWeiboData, getZhihuData, getNeteaseData } from '../page'
+
+const mcpHandler = initializeMcpApiHandler(
+  (server) => {
+    server.tool(
+      '热搜新闻榜单',
+      '今日微博、知乎、网易热搜新闻',
+      {},
+      async ({}, { sendNotification }): Promise<CallToolResult> => {
+        await sendNotification({
+          method: 'notifications/message',
+          params: {
+            level: 'info',
+            data: `正在获取热搜榜单...`,
+          },
+        })
+
+        const [weiboData, zhihuData, neteaseData] = await Promise.all([
+          getWeiboData(),
+          getZhihuData(),
+          getNeteaseData(),
+        ])
+
+        return {
+          content: [
+            {
+              type: 'text',
+              text: `微博热搜： ${weiboData.reduce(
+                (acc, curr) => `${acc}\n[${curr.title}](${curr.url})`,
+                ''
+              )}`,
+            },
+            {
+              type: 'text',
+              text: `知乎热搜： ${zhihuData.reduce(
+                (acc, curr) => `${acc}\n[${curr.title}](${curr.url})`,
+                ''
+              )}`,
+            },
+            {
+              type: 'text',
+              text: `网易热搜： ${neteaseData.reduce(
+                (acc, curr) => `${acc}\n[${curr.title}](${curr.url})`,
+                ''
+              )}`,
+            },
+          ],
+        }
+      }
+    )
+  },
+  {
+    capabilities: {
+      logging: {},
+      tools: {
+        '热搜新闻榜单': {
+          description: '今日微博、知乎、网易热搜新闻',
+        },
+      },
+    },
+  }
+)
+
+const handler = (req: Request) => {
+  return createServerResponseAdapter(req.signal, (res) => {
+    mcpHandler(req, res)
+  })
+}
+
+export { handler as GET }
+export { handler as POST }
+export { handler as DELETE }
